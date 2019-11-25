000a4edc8b102ef451ddb220b54d72a4e0ecac90
Yvan Sraka <yvan@sraka.pw>
2019-11-25 17:42:50 +0100

    First implementation (with bugs)
       Compiling playground v0.0.1 (/playground)
    error[E0308]: mismatched types
      --> src/main.rs:27:27
       |
    27 |     display(complementary(red));
       |                           ^^^
       |                           |
       |                           expected `&Color`, found struct `Color`
       |                           help: consider borrowing here: `&red`
    
    error[E0308]: mismatched types
      --> src/main.rs:29:28
       |
    29 |     complementary_in_place(red);
       |                            ^^^
       |                            |
       |                            expected `&mut Color`, found struct `Color`
       |                            help: consider mutably borrowing here: `&mut red`
    
    error: aborting due to 2 previous errors
    
    For more information about this error, try `rustc --explain E0308`.
    error: could not compile `playground`
    
    To learn more, run the command again with --verbose.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Color {
+    r: u8,
+    g: u8,
+    b: u8
+}
+
+fn complementary(color: &Color) -> Color {
+    Color {
+        r: 255 - color.r,
+        g: 255 - color.g,
+        b: 255 - color.b
+    }
+}
+
+fn complementary_in_place(color: &mut Color) {
+    color.r = 255 - color.r;
+    color.g = 255 - color.g;
+    color.b = 255 - color.b;
+}
+
+fn display(color: Color) {
+    println!("{:x} {:x} {:x}", color.r, color.g, color.b);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut red = Color { r: 255, g: 0, b: 0 };
+    display(complementary(red));
+
+    complementary_in_place(red);
+    display(red);
 }
