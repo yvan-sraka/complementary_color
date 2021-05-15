2f48b6c7b5ae05534f9d6f6d7b3a2d440fac161a
Jean-Luc <jeanluc.lys@gmail.com>
2021-05-15 12:51:29 +0200

    Debug main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ struct Color {
     b: u8
 }
 
-fn complementary(color: Color) -> Color {
+fn complementary(color: &Color) -> Color {
     Color {
         r: 255 - color.r,
         g: 255 - color.g,
@@ -12,20 +12,20 @@ fn complementary(color: Color) -> Color {
     }
 }
 
-fn complementary_in_place(mut color: Color) {
+fn complementary_in_place(mut color: &mut Color) {
     color.r = 255 - color.r;
     color.g = 255 - color.g;
     color.b = 255 - color.b;
 }
 
-fn display(color: Color) {
+fn display(color: &Color) {
     println!("{:x} {:x} {:x}", color.r, color.g, color.b);
 }
 
 fn main() {
     let mut red = Color { r: 255, g: 0, b: 0 };
-    display(complementary(red));
-
-    complementary_in_place(red);
-    display(red);
+    display(&complementary(&red));
+    let r = &mut red;
+    complementary_in_place(r);
+    display(&red);
 }
